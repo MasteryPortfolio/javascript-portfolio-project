dd9046fda74401cb9da7580d7f18ae4a9df88601
jenniferaslan <masterys@comcast.net>
2024-06-02 23:57:37 -0700

    make itinerary work for adding and removing items

--- js/script.js
+++ js/script.js
@@ -16,6 +16,10 @@ async function convertCurrency() {
 }
 
 // Itinerary Functionality
+const addItemBtn = document.getElementById('addItemBtn');
+const addItemField = document.getElementById('itemToAdd');
+const itineraryList = document.getElementById('itineraryList');
+
 class Item {
     constructor(title) {
         this.title = title;
@@ -28,16 +32,69 @@ const itinerary = {
     updateList: function (title) {
         const item = new Item(title);
         this.items.push(item);
-        console.log(`${item.title} has been added to the itinerary.`);
+        console.log(this.items);
+    },
+
+    rebuildList: function () {
+        for (let i = 0; i < this.items.length; i++) {
+            const title = this.items[i].title;
+            console.log(title);
+            addItem(title);
+        }
+    },
+
+}
+
+
+function emptyList() {
+    while (itineraryList.lastChild) {
+        itineraryList.removeChild(itineraryList.lastChild);
+    }
+    itinerary.rebuildList();
+}
+
+function deleteItem(event) {
+    const itemTitle = event.currentTarget.previousElementSibling.textContent;
+    for (let item of itinerary.items) {
+        try {
+            if (itemTitle === item.title) {
+                const index = itinerary.items.indexOf(item);
+                itinerary.items.splice(index, 1);
+                console.log(itinerary.items);
+                emptyList();
+            }
+        } catch (e) {
+            console.error(e);
+        }
     }
 }
 
-function addItem() {
-    const title = prompt('Enter itinerary item');
-    console.log(title);
+function clickToAdd() {
+    title = addItemField.value;
     itinerary.updateList(title);
-    console.log(itinerary.items);
+    addItem(title);
+    addItemField.value = '';
 }
 
+function addItem(title) {
 
-//document.querySelector('#addItemBtn').addEventListener('click', addItem);
+    const newItem = document.createElement('li');
+    const newItemText = document.createElement('span');
+    const deleteBtn = document.createElement('button');
+
+    newItem.draggable = true;
+    newItem.className = 'list-item';
+    itineraryList.appendChild(newItem);
+
+    newItemText.textContent = title;
+    newItem.appendChild(newItemText);
+
+    deleteBtn.textContent = 'x';
+    deleteBtn.className = 'btn-delete';
+    deleteBtn.title = 'Remove item';
+    newItem.appendChild(deleteBtn);
+    deleteBtn.addEventListener('click', deleteItem);
+}
+
+addItemBtn.addEventListener('click', clickToAdd);
+
